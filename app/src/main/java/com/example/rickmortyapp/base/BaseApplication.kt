8bad57681fe027157379350aecfc19c42f3c4884
rithwanul <rithwanul@gmail.com>
2023-09-01 22:53:52 +0600

    Configure timber and add glide dependencies

--- app/src/main/java/com/example/rickmortyapp/base/BaseApplication.kt
+++ app/src/main/java/com/example/rickmortyapp/base/BaseApplication.kt
@@ -1,12 +1,21 @@
 package com.example.rickmortyapp.base
 
+import com.example.rickmortyapp.BuildConfig
 import com.example.rickmortyapp.di.component.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
+import timber.log.Timber
 
 class BaseApplication: DaggerApplication() {
 
 
     override fun applicationInjector():
             AndroidInjector<out DaggerApplication> = DaggerAppComponent.builder().create(this);
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 }
